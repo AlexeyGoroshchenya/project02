875d0aa7add1810e202cb88a2cef462c45b2fcef
Alexey Goroshchenya <375295509897@yandex.ru>
2022-02-27 17:14:46 +0300

    выполнил обязательное 21

--- src/modules/validation.js
+++ src/modules/validation.js
@@ -1,5 +1,48 @@
 const validation = () => {
 
+    const calcInput = document.querySelectorAll('.calc-block input ');
+
+    const textInputs = [
+        document.getElementById('form1-name'),
+        document.getElementById('form2-name'),
+        document.getElementById('form3-name'),
+        document.getElementById('form2-message'),
+    ]
+    const emailInputs = [
+        document.getElementById('form1-email'),
+        document.getElementById('form2-email'),
+        document.getElementById('form3-email'),
+    ]
+    const telInputs = [
+        document.getElementById('form1-phone'),
+        document.getElementById('form2-phone'),
+        document.getElementById('form3-phone'),
+    ]
+
+
+    calcInput.forEach((element) => {
+        element.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/\D+/, "");
+        })
+    })
+
+    textInputs.forEach((element) => {
+        element.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^а-яА-я\s\-]/, "");
+        })
+    })
+
+    telInputs.forEach((element) => {
+        element.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^0-9\(\)\-]/, "");
+        })
+    })
+
+    emailInputs.forEach((element) => {
+        element.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^\w\-\.\_\~\!\*\'\@]/, "");
+        })
+    })
 
 }
 
